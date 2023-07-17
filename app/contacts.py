12e01a7fe1de7eaacf6c15719f1818b71954833a
steven <stevgomezt@gmail.com>
2023-07-17 16:56:07 -0500

    avances

--- app/contacts.py
+++ app/contacts.py
@@ -4,7 +4,7 @@ from db import mysql
 contacts = Blueprint('contacts', __name__, template_folder='app/templates')
 
 
-@contacts.route('/')
+@contacts.route('/index')
 def Index():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM contacts')
@@ -13,16 +13,49 @@ def Index():
     return render_template('index.html', contacts=data)
 
 
+@contacts.route('/list', methods=['POST', 'GET'])
+def list():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM contacts')
+    data = cur.fetchall()
+    cur.close()
+    return render_template('list-contacts.html', contacts=data)
+
+
 @contacts.route('/add_contact', methods=['POST'])
 def add_contact():
     if request.method == 'POST':
+        numero_documento = request.form['numero_documento']
         nombre = request.form['nombre']
-        telefono = request.form['telefono']
+        edad = request.form['edad']
+        genero = request.form['genero']
+        estado_civil = request.form['estado_civil']
         correo = request.form['correo']
+        telefono = request.form['telefono']
+        nivel_estudios = request.form['nivel_estudios']
+        estrato = request.form['estrato']
+        num_hijos = request.form['num_hijos']
+        personas_cargo = request.form['personas_cargo']
+        experiencia = request.form['experiencia']
+        area_experiencia = request.form['area_experiencia']
+        tiempo_ventas = request.form['tiempo_ventas']
+        experiencia_general = request.form['experiencia_general']
+        otra_area_experiencia = request.form['otra_area_experiencia']
+        pda_color = request.form['pda_color']
         try:
             cur = mysql.connection.cursor()
             cur.execute(
-                "INSERT INTO contacts (nombre, telefono, correo) VALUES (%s,%s,%s)", (nombre, telefono, correo))
+                """INSERT INTO contacts 
+                (numero_documento, nombre, edad, genero, estado_civil, correo, 
+                telefono, nivel_estudios, estrato, num_hijos, personas_cargo, 
+                experiencia, area_experiencia, tiempo_ventas, experiencia_general, 
+                otra_area_experiencia, pda_color) 
+                VALUES 
+                (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                (numero_documento, nombre, edad, genero, estado_civil,
+                 correo, telefono, nivel_estudios, estrato, num_hijos, personas_cargo,
+                 experiencia, area_experiencia, tiempo_ventas, experiencia_general,
+                 otra_area_experiencia, pda_color))
             mysql.connection.commit()
             flash('Contact Added successfully')
             return redirect(url_for('contacts.Index'))
@@ -44,17 +77,47 @@ def get_contact(id):
 @contacts.route('/update/<id>', methods=['POST'])
 def update_contact(id):
     if request.method == 'POST':
+        numero_documento = request.form['numero_documento']
         nombre = request.form['nombre']
-        telefono = request.form['telefono']
+        edad = request.form['edad']
+        genero = request.form['genero']
+        estado_civil = request.form['estado_civil']
         correo = request.form['correo']
+        telefono = request.form['telefono']
+        nivel_estudios = request.form['nivel_estudios']
+        estrato = request.form['estrato']
+        num_hijos = request.form['num_hijos']
+        personas_cargo = request.form['personas_cargo']
+        experiencia = request.form['experiencia']
+        area_experiencia = request.form['area_experiencia']
+        tiempo_ventas = request.form['tiempo_ventas']
+        experiencia_general = request.form['experiencia_general']
+        otra_area_experiencia = request.form['otra_area_experiencia']
+        pda_color = request.form['pda_color']
         cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE contacts
-            SET nombre = %s,
+        cur.execute(
+            """UPDATE contacts
+            SET numero_documento = %s,
+                nombre = %s,
+                edad = %s,
+                genero = %s,
+                estado_civil = %s,
                 correo = %s,
-                telefono = %s
-            WHERE id = %s
-        """, (nombre, correo, telefono, id))
+                telefono = %s,
+                nivel_estudios = %s,
+                estrato = %s,
+                num_hijos = %s,
+                personas_cargo = %s,
+                experiencia = %s,
+                area_experiencia = %s,
+                tiempo_ventas = %s,
+                experiencia_general = %s,
+                otra_area_experiencia = %s,
+                pda_color = %s
+            WHERE id = %s """,
+            (numero_documento, nombre, edad, genero, estado_civil, correo, telefono, nivel_estudios, estrato,
+             num_hijos, personas_cargo, experiencia, area_experiencia, tiempo_ventas, experiencia_general,
+             otra_area_experiencia, pda_color, id))
         flash('Contact Updated Successfully')
         mysql.connection.commit()
         return redirect(url_for('contacts.Index'))
